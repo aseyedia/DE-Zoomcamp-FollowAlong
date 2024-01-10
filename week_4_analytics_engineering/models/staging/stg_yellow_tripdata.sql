31a69ec56dc754b3b09ba296bc9939d79fe24000
Arta Seyedian <arta.seyedian@gmail.com>
2024-01-10 17:31:25 +0000

    fixed all sql changes

--- week_4_analytics_engineering/models/staging/stg_yellow_tripdata.sql
+++ week_4_analytics_engineering/models/staging/stg_yellow_tripdata.sql
@@ -2,7 +2,7 @@
 
 with
     tripdata as (
-        select *, row_number() over (partition by vendorid, tpep_pickup_datetime) as rn
+        select *, row_number() over (partition by cast(vendorid as integer), tpep_pickup_datetime) as rn
         from {{ source("staging", "yellow_tripdata") }}
         where vendorid is not null
     )
